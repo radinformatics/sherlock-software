2f7a6afb90683e873d6d4be468b3965e825f969e
vsoch <vsochat@stanford.edu>
2016-10-01 17:36:59 -0700

    modified:   anaconda2.lua 	modified:   anaconda3.lua

--- anaconda2.lua
+++ anaconda2.lua
@@ -1,5 +1,2 @@
-conflict python
-module-whatis   "Anaconda (2) for Python"
 prepend_path("PATH","/share/PI/langlotz/software/anaconda2/bin")
 help([[ This module provides support for the Anaconda 2 distribution]])
-
